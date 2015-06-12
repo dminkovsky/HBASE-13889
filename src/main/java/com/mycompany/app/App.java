df16232559fed62974905cf348ceb612b28c612f
Dmitry Minkovsky <dminkovsky@gmail.com>
2015-06-12 15:21:39 -0400

    HBASE-13889: Fails compile with org.apache.hadoop.hbase.client.Put

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,21 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.TableName;
 
 public class App {
     public static void main(String[] args) throws java.io.IOException {
         Configuration config = HBaseConfiguration.create();
         Connection connection = ConnectionFactory.createConnection(config);
+        Table table = connection.getTable(TableName.valueOf("users"));
+        Put put = new Put(Bytes.toBytes("hello"));
+        put.addColumn(
+            Bytes.toBytes("d"),
+            Bytes.toBytes("name"),
+            Bytes.toBytes("user")
+        );
     }
 }
